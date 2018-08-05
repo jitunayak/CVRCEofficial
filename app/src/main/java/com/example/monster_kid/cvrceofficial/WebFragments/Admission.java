2a766cdf9ea0c4fea7de2ca508382de1eb3c76bf
Jitu nayak <jitunayak715@gmail.com>
2018-08-05 13:20:36 +0530

    edited nav bar

---
+++ app/src/main/java/com/example/monster_kid/cvrceofficial/WebFragments/Admission.java
@@ -0,0 +1,41 @@
+package com.example.monster_kid.cvrceofficial.WebFragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+
+import com.example.monster_kid.cvrceofficial.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Admission extends Fragment {
+
+WebView mWebView;
+    public Admission() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v=inflater.inflate(R.layout.content_main, container, false);
+        mWebView = (WebView) v.findViewById(R.id.webview_id);
+        mWebView.loadUrl("http://googleweblight.com/?lite_url=http://cvrce.edu.in/AdmissionEnquiryForm.php");
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mWebView.setWebViewClient(new WebViewClient());
+        mWebView.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
+
+        return v;
+    }
+
+}
---
+++ app/src/main/java/com/example/monster_kid/cvrceofficial/WebFragments/HomePage.java
@@ -0,0 +1,42 @@
+package com.example.monster_kid.cvrceofficial.WebFragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+
+import com.example.monster_kid.cvrceofficial.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomePage extends Fragment {
+
+WebView mWebView;
+    public HomePage() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v=inflater.inflate(R.layout.content_main, container, false);
+        mWebView = (WebView) v.findViewById(R.id.webview_id);
+        //mWebView.loadUrl("http://googleweblight.com/?lite_url=http://cvrce.edu.in/");
+        mWebView.loadUrl("http://cvrce.edu.in/");
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+       mWebView.setWebViewClient(new WebViewClient());
+        mWebView.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
+        return v;
+
+    }
+
+}
